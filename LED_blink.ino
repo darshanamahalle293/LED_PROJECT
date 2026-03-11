3aa4ed5f03b577ae80880ba2b26d731feb293c6a
darshanamahalle293 <darshanamahalle95@gmail.com>
2026-03-11 21:20:28 +0530

    Commit_2

---
+++ LED_blink.ino
@@ -0,0 +1,17 @@
+// This is my First IOT Program
+#define my_led 7 //global declaration, LED pin no.7
+
+
+void setup() {
+  //initialize digital pin LED_BUTTON as an output
+  pinMode(my_led, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(my_led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                  //wait for a second
+  digitalWrite(my_led, LOW);    //turn the LED off by making the voltage LOW 
+  delay(1000);                  //wait for a second
+                   
+}
